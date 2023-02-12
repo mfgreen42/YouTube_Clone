7330dd2d24929b9ab9ff0832b28822c777fd44a1
Mara Green <maragreen42@gmail.com>
2023-02-11 18:30:24 -0800

    work on Home Page+adding search bar and display videos

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = {
+urlpatterns = [
     path('', views.video_comments),
     path('<str:video_id>/', views.get_comments_by_video),
     path('user/<str:video_id>/', views.user_comments)
-}
+]
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -9,32 +9,41 @@ const HomePage = () => {
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+  const [video, setVideo] = useState([]);
 
   useEffect(() => {
-    const fetchCars = async () => {
+    const fetchVideos = async () => {
       try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+        let response = await axios.get("https://www.googleapis.com/youtube/v3/search?q=&key=AIzaSyA6HGDmTmHv29DCMk4vhNy4siE7wRJwH1I&part=snippet&type=video&maxResults=6", {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        setCars(response.data);
+        setVideo(response.data);
       } catch (error) {
         console.log(error.response.data);
       }
     };
-    fetchCars();
+    fetchVideos();
   }, [token]);
   return (
     <div className="container">
       <h1>Home Page for {user.username}!</h1>
-      {cars &&
+      {/* {cars &&
         cars.map((car) => (
           <p key={car.id}>
             {car.year} {car.model} {car.make}
           </p>
-        ))}
+        ))} */}
+        <label>
+          Search: 
+          <input
+          type = 'text'
+          name = 'search'
+          value = ''
+          onChange={handleInputChange}
+          />
+        </label>
     </div>
   );
 };

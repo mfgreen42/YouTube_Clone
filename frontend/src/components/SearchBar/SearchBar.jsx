5f69ca403c47e94bb9ae76735531b6f24a1441e6
Mara Green <maragreen42@gmail.com>
2023-02-12 16:15:03 -0800

    SearchBar displays thumbnail when acitivated + add VideoPage.js

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -8,40 +8,49 @@ const SearchBar = () => {
 
     const [videos, setVideos] = useState([])
     const [searchTerm, setSearchTerm] = useState('');
-    const [filteredVideos, setFilteredVideos] = useState([]);
+    const [searchClicked, setSearchClicked] = useState(false);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+
+
 
     useEffect(() => {
         async function getVidoes() {
           try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users?maxResults=6');
-            setVideos(response.data);
+            const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=AIzaSyA6HGDmTmHv29DCMk4vhNy4siE7wRJwH1I&part=snippet&type =video&maxResults=6`);
+            setVideos(response.data.items);
+            console.log('search has ran')
           } catch (error) {
             console.error(error);
           }
     }
 
+      if (searchClicked){ 
       getVidoes();
-},[])
+      }
+},[searchTerm, searchClicked])
 
-const handleSearch = () => {
-    const filtered = videos.filter(video =>
-      video.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredVideos(filtered);
-  };
+// const handleSearch = () => {
+//     const filtered = videos.filter(video =>
+//       video.name.toLowerCase().includes(searchTerm.toLowerCase())
+//     );
+//     setVideos(filtered);
+//   };
 
   return (
     <div>
+      <h2>Search Results</h2>
       <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
-      <button onClick={handleSearch}>Search</button>
-      <button onClick={() => setSearchTerm('')}>Reset Search</button>
-      <ul id="video-list">
-        {filteredVideos.map(videos => (
-          <li key={videos.id}>{videos.name} ({videos.email})</li>
+      <button onClick={() => setSearchClicked(true)}>Search</button>
+      <div id="video-grid">
+        {videos.map(video => (
+          <a key={video.id.videoId} href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank">
+            <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+            <p>{video.snippet.title}</p>
+          </a>
         ))}
-      </ul>
+      </div>
     </div>
-  );
+    );
 }
 
 
